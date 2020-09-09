c13a389334447d2f7aad18d5b5c58de614246494
Stefan Balas <stefan.paul.balas@ibm.com>
2020-09-09 03:07:39 +0300

    Fix style

--- src/App.js
+++ src/App.js
@@ -25,13 +25,13 @@ class App extends Component {
       .post("https://kickbro-landing.herokuapp.com/email", {
         email: this.state.email,
       })
-      .then( (response) => {
+      .then( () => {
         this.setState({
           isJoined: true,
           isInvalid: false,
         });
       })
-      .catch( (error) => {
+      .catch( () => {
           this.setState({
             isJoined: false,
             isInvalid: true
@@ -45,20 +45,20 @@ class App extends Component {
 
     return (
       <div className={classes.content}>
-        <div className={classes.Landing}></div>
+        <div className={classes.Landing}/>
 
         <div className={classes.instagram}>
-        <a href="http://instagram.com/kickbro_worldwide">@kickbro <img src={instagram} alt="instagram"/></a>
+          <a href="http://instagram.com/kickbro_worldwide">@kickbro <img src={instagram} alt="instagram"/></a>
         </div>
         <div className={classes.font}>
-          If you asked yourself “How many steps I made  with these <TypeIt className={classes.nike} options={{ loop: true, speed: 200 }}>Nike's</TypeIt>
-          ”<br></br> then this app is for you. Join our beta.
+            {`If you asked yourself:\n“How many steps I made \nwith these `}
+            <TypeIt className={classes.nike} options={{ loop: true, speed: 200 }}>{`Nike's`}</TypeIt>
+            {`?”\nThen this app is for you. \nJoin our beta.`}
         </div>
         {this.state.isInvalid ? 
           errorMessage : null}
         {!this.state.isJoined ?
           <div className={classes.inputGroup}>
-            
               <input
                 className={classes.input}
                 type="text"
@@ -69,16 +69,14 @@ class App extends Component {
               <button className={classes.button} onClick={this.onJoin}>
                 Join beta
               </button>
-              
-             
           </div> : successMessage}
         <div className={classes.bottom}>
           <div className={classes.discord}>
-            <img src={discord} alt="discord"/><a href="https://discord.gg/PfPgUwg"> Join our discord comunity</a>
+            <img src={discord} alt="discord"/><a href="https://discord.gg/PfPgUwg"> Join our discord community</a>
           </div>
           <div className={classes.imageGroup}>
-            <div className={classes.phoneCase}></div>
-            <div className={classes.phoneScreen}></div>
+            <div className={classes.phoneCase}/>
+            <div className={classes.phoneScreen}/>
           </div>
         </div>
       </div>
