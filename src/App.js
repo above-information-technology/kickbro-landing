e0b1c8006e835c9901b7615a7b853f6be28330a7
Stefan Danescu <stefi_d27@yahoo.com>
2020-09-09 13:59:07 +0300

    first deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import classes from "./App.css";
 
 import instagram from "./assets/instagram.png";
 import discord from "./assets/discord.png";
-import TypeIt from 'typeit-react';
+// import TypeIt from 'typeit-react';
+import TypeIt from "typeit";
 
 const axios = require("axios");
 
@@ -12,6 +13,7 @@ class App extends Component {
     email: "",
     isJoined: false,
     isInvalid: false,
+    isAlreadyJoined: false
   };
 
   changeEmail = (event) => {
@@ -29,19 +31,58 @@ class App extends Component {
         this.setState({
           isJoined: true,
           isInvalid: false,
+          isAlreadyJoined: false
         });
       })
-      .catch( () => {
-          this.setState({
-            isJoined: false,
-            isInvalid: true
-          });
+      .catch( (e) => {
+          if (e.message.includes('status code 400')) {
+            this.setState({
+              isJoined: false,
+              isInvalid: true,
+              isAlreadyJoined: false
+            });
+          }
+          if (e.message.includes('status code 404')) {
+            this.setState({
+              isJoined: false,
+              isInvalid: false,
+              isAlreadyJoined: true
+            });
+          }
+
       });
   };
 
+  componentDidMount () {
+    new TypeIt(this.el, {
+      speed: 150,
+      loop: true,
+    })
+    .type("Nike's")
+    .pause(1500)
+    .delete()
+    .pause(500)
+    .type("Adidas")
+    .pause(1500)
+    .delete()
+    .pause(500)
+    .type("New Balance")
+    .pause(1500)
+    .delete()
+    .pause(500)
+    .type("Air Jordan")
+    .pause(1500)
+    .delete()
+    .type("Puma")
+    .pause(1500)
+    .delete()
+    .go();
+  }
+
   render() {
     let  successMessage = (<div className={classes.notification}>You successfully signed up. Stay tuned, there are news to come.</div>);
-    let errorMessage = (<div className={classes.error}>Invalid email address</div>);
+    let errorMessage = (<div className={classes.error}>Wrong email address</div>);
+    let alreadyJoinedMessage = (<div className={classes.notification}>You've already joined our beta. Stay tuned, there are news to come.</div>);
 
     return (
       <div className={classes.content}>
@@ -52,25 +93,32 @@ class App extends Component {
         </div>
         <div className={classes.font}>
             {`If you asked yourself:\n“How many steps I made \nwith these `}
-            <TypeIt className={classes.nike} options={{ loop: true, speed: 200 }}>{`Nike's`}</TypeIt>
+            <span className={classes.nike} ref={(el) => { this.el = el; }}></span>
+            
+
             {`?”\nThen this app is for you. \nJoin our beta.`}
         </div>
 
         {!this.state.isJoined ?
-          <div className={classes.inputGroup}>
-              <input
-                className={classes.input}
-                type="text"
-                placeholder="enter your email here"
-                value={this.state.email}
-                onChange={this.changeEmail}
-                isInvalid={this.state.isInvalid ? "true" : "false"}
-              />
-              {this.state.isInvalid ? errorMessage : null}
-              <button className={classes.button} onClick={this.onJoin}>
-                Join beta
-              </button>
-          </div> : successMessage}
+          this.state.isAlreadyJoined ? 
+            alreadyJoinedMessage
+            :
+            (
+              <div className={classes.inputGroup}>
+                  <input
+                    className={classes.input}
+                    type="text"
+                    placeholder="enter your email here"
+                    value={this.state.email}
+                    onChange={this.changeEmail}
+                    isinvalid={this.state.isInvalid ? "true" : "false"}
+                  />
+                  {this.state.isInvalid ? errorMessage : null}
+                  <button className={classes.button} onClick={this.onJoin}>
+                    Join beta
+                  </button>
+              </div>
+            ) : successMessage }
         <div className={classes.bottom}>
           <div className={classes.discord}>
             <img src={discord} alt="discord"/><a href="https://discord.gg/PfPgUwg"> Join our discord community</a>
